ef24411a2bb56343d6d18cad810e9c2cd0f17eba
Sergey Nartimov <sergey.nartimov@toptal.com>
2015-07-16 11:28:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ESLint Config
+
+Install `eslint-config-toptal` package:
+
+```shell
+npm install --save-dev toptal/eslint-config-toptal
+```
+
+Use the config in `.eslintrc`:
+
+```json
+{
+  "extends": "toptal"
+}
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  rules: {
+    // Possible Errors
+    "no-console": 0,
+    // Best Practices
+    "dot-location": [2, "property"],
+    "no-self-compare": 2,
+    "radix": 2,
+    "wrap-iife": 2,
+    // Stylistic Issues
+    "array-bracket-spacing": [2, "never"],
+    "brace-style": [2, "1tbs", {"allowSingleLine": true}],
+    "comma-style": [2, "last"],
+    "computed-property-spacing": [2, "never"],
+    "indent": [2, 2],
+    "linebreak-style": [2, "unix"],
+    "no-multiple-empty-lines": [2, {"max": 1}],
+    "no-nested-ternary": 2,
+    "no-underscore-dangle": 0,
+    "no-unneeded-ternary": 2,
+    "object-curly-spacing": [2, "never"],
+    "operator-assignment": [2, "always"],
+    "operator-linebreak": [2, "after"],
+    "padded-blocks": [2, "never"],
+    "quotes": [2, "single", "avoid-escape"],
+    "semi": [2, "never"],
+    "space-after-keywords": [2, "always"],
+    "space-before-blocks": [2, "always"],
+    "space-before-function-paren": [2, "never"],
+    "space-in-parens": [2, "never"],
+    "spaced-comment": [2, "always"],
+    // ECMAScript 6
+    "no-this-before-super": 2,
+    "no-var": 2,
+    "object-shorthand": [2, "always"],
+    "prefer-const": 2
+  }
+}
